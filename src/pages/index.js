adfae23b50cac8066d87f78d645b434efc2f416c
David-EV <davidenriquez72@hotmail.com>
2021-10-26 23:35:52 -0500

    Termino tutorial Gatsby parte 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,16 @@
 import * as React from 'react'
 //import { Link } from 'gatsby'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 // Step 2: Define your component
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="https://crehana-blog.imgix.net/media/filer_public/4f/fe/4ffe7456-b63b-4e0f-8fd3-fc619bc0e345/habilidades-de-un-programador.jpg"
+      />
     </Layout>
   )
 }
